0617d5cfdd33d7f7cd253ee2c86b3eab24f806a9
Tayou <tayou@gmx.net>
2021-01-02 16:38:58 +0100

    added Chess Figure Image loading and sample image

--- Project/src/Figure.java
+++ Project/src/Figure.java
@@ -1,5 +1,69 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Figure {
-
+	
+	Image image;
+	
 	private FigureType type;
-
+	
+	public Figure(FigureType type) {
+		this.type = type;
+	}
+	
+	private BufferedImage getImage(String filename) {
+		// This time, you can use an InputStream to load
+		try {
+			// Grab the InputStream for the image.
+			InputStream in = Files.newInputStream(Path.of(filename));
+			
+			// Then read it.
+			return ImageIO.read(in);
+		} catch (IOException e) {
+			System.out.println("The image was not loaded.");
+			//System.exit(1);
+		}
+		
+		return null;
+	}
+	
+	public ImageIcon getSingleImage() {
+		BufferedImage srcImage = getImage("chess_2.png");
+		int oneFigureSize = srcImage.getWidth()/6;
+		
+		
+		switch (type) {
+			case KING -> {
+				srcImage = cropImage(srcImage, new Rectangle(oneFigureSize*5, 0, oneFigureSize, oneFigureSize));
+			}
+			case QUEEN -> {
+				srcImage = cropImage(srcImage, new Rectangle(oneFigureSize*4, 0, oneFigureSize, oneFigureSize));
+			}
+			case ROOK -> {
+				srcImage = cropImage(srcImage, new Rectangle(oneFigureSize*3, 0, oneFigureSize, oneFigureSize));
+			}
+			case BISHOP -> {
+				srcImage = cropImage(srcImage, new Rectangle(oneFigureSize*2, 0, oneFigureSize, oneFigureSize));
+			}
+			case KNIGHT -> {
+				srcImage = cropImage(srcImage, new Rectangle(oneFigureSize, 0, oneFigureSize, oneFigureSize));
+			}
+			case PAWN -> {
+				srcImage = cropImage(srcImage, new Rectangle(0, 0, oneFigureSize, oneFigureSize));
+			}
+		}
+		
+		return new ImageIcon(srcImage);
+	}
+	
+	private BufferedImage cropImage(BufferedImage src, Rectangle rect) {
+		BufferedImage dest = src.getSubimage(rect.x, rect.y, rect.width, rect.height);
+		return dest;
+	}
 }
